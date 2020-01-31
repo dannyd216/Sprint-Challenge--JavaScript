20cd8f3024eadde7fb6cbb529406fa531a62a9ab
dannyd216 <dparrish216@gmail.com>
2020-01-31 16:23:20 -0700

    functions page done

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,3 +1,4 @@
+
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function
@@ -7,7 +8,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
-const consume (a, b, cb) = function ()
+let consume = function (a, b, cb) {
+  return cb(a,b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -16,11 +19,17 @@ const consume (a, b, cb) = function ()
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (num1,  num2) => num1 + num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`;
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -28,6 +37,8 @@ const consume (a, b, cb) = function ()
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
+
+//closures give us the ability to nest functions together and allow us to cascade down inheritances so any functions nested under will be able to access anytime
 
 
 const external = "I'm outside the function";
@@ -42,3 +53,7 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+
+
+
